dbf8366edb30dac8fc3099146407ad869b0b6b51
Masamitsu Oikawa <oicawa@gmail.com>
2015-06-19 06:47:07 +0900

    Added basic data files.

---
+++ resources/public/data/6e851190-81e3-4b89-9304-a06c6282630c/operations.js
@@ -0,0 +1,19 @@
+define(function (require) {
+  require("jquery");
+  //require("json2");
+  //require("jquery_ui");
+  require("jquery_splitter");
+  require("jsrender");
+  var Utils = require("core/Utils");
+  //var Toolbar = require("core/controls/Toolbar/Toolbar");
+  //var Grid = require("core/controls/Grid/Grid");
+  //var Detail = require("core/controls/Detail/Detail");
+  return {
+    "add_new_field" : function() {
+      alert("add new field.");
+    },
+    "delete_field" : function() {
+      alert("delete field.");
+    }
+  };
+});
