34aa55eaca2a2ca74d442ccd9e10a518ae331c18
oicawa <oicawa@gmail.com>
2016-08-25 01:04:30 +0900

    Added DateTime control.

---
+++ resources/public/core/Control/Field/DateTime.js
@@ -0,0 +1,61 @@
+define(function (require) { 
+  require("jquery");
+  var Utils = require("core/Utils");
+  var Inherits = require("core/Inherits");
+  var Field = require("core/Control/Field/Field");
+  
+  var TEMPLATE = '<label></label><div><input style="color:black;"/></div>';
+  
+  function DateTime() {
+    Field.call(this, "core/Control/Field", "DateTime");
+    this._input = null;
+    this._value = null;
+  };
+  Inherits(DateTime, Field);
+  
+  DateTime.prototype.init = function(selector, field) {
+    var dfd = new $.Deferred;
+    var root = $(selector);
+
+    // Create form tags
+    var self = this;
+    
+    root.append(TEMPLATE);
+    var label = root.find("label");
+    var caption = field.label;
+    label.text(caption);
+    self._input = root.find("input");
+    self._input.attr("name", field.name);
+    self._input.w2field("date");
+    
+    dfd.resolve();
+    return dfd.promise();
+  };
+
+  DateTime.prototype.backuped = function() {
+    return this._value;
+  };
+
+  DateTime.prototype.commit = function() {
+    this._value = this._input.val();
+  };
+
+  DateTime.prototype.restore = function() {
+    this._input.val(this._value);
+  };
+
+  DateTime.prototype.edit = function(on) {
+    this._input.attr("readonly", !on);
+  };
+
+  DateTime.prototype.data = function(value) {
+    if (arguments.length == 0) {
+      return this._input.val();
+    } else {
+      this._input.val(value);
+      this._value = value;
+    }
+  };
+  
+  return DateTime;
+}); 
