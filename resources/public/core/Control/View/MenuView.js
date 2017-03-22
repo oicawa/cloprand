1ef0259337cad164302a228cf789a046056a349e
oicawa <oicawa@gmail.com>
2017-03-22 23:52:18 +0900

    Removed custom search form.

--- resources/public/core/Control/View/MenuView.js
+++ resources/public/core/Control/View/MenuView.js
@@ -73,50 +73,44 @@ define(function (require) {
     var columns = null;
     $.when(
       Utils.load_css("/core/Control/View/MenuView.css"),
-      Storage.read(Class.CLASS_ID)
-      .done(function(data) {
-        classes = data;
-        self._class = classes[Class.CLASS_ID];
-      })
+      Storage.read(Class.CLASS_ID).done(function(data) { classes = data; })
     )
     .then(function() {
-      return Grid.create_columns(self._class)
-      .done(function(columns_) {
-        columns = columns_;
-      })
+      self._class = classes[Class.CLASS_ID];
+      return Grid.create_columns(self._class).done(function(columns_) { columns = columns_; });
     })
-    .always(function() {
+    .then(function() {
       view.append(TEMPLATE);
-      self._grid.init(grid_selector, columns)
-      .then(function () {
-        self._grid.add_operation("dblclick", function(event) {
-          MenuView.show_gridview(self, event.recid);
-        });
-        self._grid.select_column(true);
-        self._grid.toolbar(true);
-        
-        function search_generator(item) {
-          var template = "<div id={{ID}} style='margin:0px 5px 0px 5px;'><i class='{{ICON}}'style='margin:2px;'/><input type='text' class='w2ui-grid w2ui-toolbar-search'/></div>";
-          var id = Uuid.version4();
-          var html = template.replace(/{{ID}}/, id)
-                             .replace(/{{ICON}}/, item.icon);
-          var selector = "#" + id + " > input";
-          console.log("search selector: [" + selector + "]");
-          $(document).off("keyup", selector);
-          $(document).on("keyup", selector, function (event) {
-            console.log("Current Text:[" + $(selector).val() + "]");
-          });
-          return html;
-        }
-        self._grid.actions([
-          { id:"search", type:"html",   text:"Search", icon:"fa fa-search",   html:search_generator }
-        ]);
-        var menus = Object.keys(classes)
-          .map(function(id) { return classes[id]; })
-          .filter(function(class_) { return class_.application == true; });
-        self._grid.data(menus);
-        self.refresh();
+      return self._grid.init(grid_selector, columns);
+    })
+    .then(function () {
+      self._grid.add_operation("dblclick", function(event) {
+        MenuView.show_gridview(self, event.recid);
       });
+      self._grid.select_column(true);
+      self._grid.toolbar(true);
+
+      function search_generator(item) {
+        var template = "<div id={{ID}} style='margin:0px 5px 0px 5px;'><i class='{{ICON}}'style='margin:2px;'/><input type='text' class='w2ui-grid w2ui-toolbar-search'/></div>";
+        var id = Uuid.version4();
+        var html = template.replace(/{{ID}}/, id)
+                           .replace(/{{ICON}}/, item.icon);
+        var selector = "#" + id + " > input";
+        console.log("search selector: [" + selector + "]");
+        $(document).off("keyup", selector);
+        $(document).on("keyup", selector, function (event) {
+          console.log("Current Text:[" + $(selector).val() + "]");
+        });
+        return html;
+      }
+      //self._grid.actions([
+      //  { id:"search", type:"html",   text:"Search", icon:"fa fa-search",   html:search_generator }
+      //]);
+      var menus = Object.keys(classes)
+        .map(function(id) { return classes[id]; })
+        .filter(function(class_) { return class_.application == true; });
+      self._grid.data(menus);
+      self.refresh();
     });
   };
   
