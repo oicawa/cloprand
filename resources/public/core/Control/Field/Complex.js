3d23a6a2a1d94c28e9e7d0cfb076d9a5112e8fd6
oicawa <oicawa@gmail.com>
2017-09-12 01:07:48 +0900

    Implementing show mode (Collapse, Popup, Embedded).

--- resources/public/core/Control/Field/Complex.js
+++ resources/public/core/Control/Field/Complex.js
@@ -8,28 +8,92 @@ define(function (require) {
   var Detail = require("core/Control/Detail");
   var Field = require("core/Control/Field/Field");
   
-  var TEMPLATE = '<div class="complex"><i></i><div class="detail"></div></div>';
+  var TEMPLATE = '<div class="complex"></div>';
+  var BUTTON_TEMPLATE = "<i></i>";
+  var DETAIL_TEMPLATE = "<div class='detail'></div>";
 
   function Complex() {
     Field.call(this, "core/Control/Field", "Complex");
+    this._root = null;
+    this._button = null;
     this._detail = null;
   };
   Inherits(Complex, Field);
 
 
   function create_collapse(self, selector) {
-    self._collapse = $(selector);
-    self._collapse.collapse = true;
+    var complex = $(selector);
+    complex.append(BUTTON_TEMPLATE);
+    self._button = $(selector + " > i");
     // caret-right, caret-down
-    self._collapse.addClass("fa");
-    self._collapse.addClass("fa-caret-right");
-    self._collapse.on("click", function (event) {
-      self._collapse.collapse = !self._collapse.collapse;
-      var old_class = self._collapse.collapse ? "fa-caret-down" : "fa-caret-right";
-      var new_class = self._collapse.collapse ? "fa-caret-right" : "fa-caret-down";
-      self._collapse.removeClass(old_class);
-      self._collapse.addClass(new_class);
+    self._button.addClass("fa");
+    self._button.addClass("fa-caret-right");
+    self._button.on("click", function (event) {
+      if (self._detail == null) {
+        // Create
+        complex.append(DETAIL_TEMPLATE);
+        self._detail = new Detail();
+        self._detail.init(selector + " > div.detail", self._class.object_fields)
+        .always(function() {
+          self._button.removeClass("fa-caret-right");
+          self._button.addClass("fa-caret-down");
+          self._detail.visible(true);
+          self._detail.edit(self._edit);
+          self._detail.data(self._data);
+        });
+        return;
+      }
+      
+      var old_class = self._detail.visible() ? "fa-caret-right" : "fa-caret-down";
+      var new_class = self._detail.visible() ? "fa-caret-down" : "fa-caret-right";
+      self._button.removeClass(old_class);
+      self._button.addClass(new_class);
       self._detail.visible(self._collapse.collapse ? false : true);
+    });
+  }
+
+  function create_popup(self, selector) {
+    var complex = $(selector);
+    complex.append(BUTTON_TEMPLATE);
+    self._button = $(selector + " > i");
+    // caret-right, caret-down
+    self._button.addClass("fa");
+    self._button.addClass("fa-ellipsis-h");
+    self._button.on("click", function (event) {
+      var detail = new Detail();
+      var dialog = new Dialog();
+      dialog.init(function(id) {
+        var dfd = new $.Deferred;
+        detail.init('#' + id, self._class.object_fields)
+        .then(function () {
+          detail.data(self._data);
+          detail.edit(self._edit);
+          detail.refresh();
+          detail.visible(true);
+          dfd.resolve();
+        });
+        return dfd.promise();
+      }).then(function () {
+        dialog.title(Locale.translate(self._class.label));
+        dialog.buttons([
+          {
+            text : "OK",
+            click: function (event) {
+              self._data = detail.data();
+              dialog.close();
+              return false;
+            }
+          },
+          {
+            text : "Cancel",
+            click: function (event) {
+              dialog.close();
+              return false;
+            }
+          }
+        ]);
+        dialog.open();
+      });
     });
   }
   
@@ -43,22 +107,38 @@ define(function (require) {
 
     // Load template data & Create form tags
     var template = null;
-    var class_ = null;
     var self = this;
     Storage.read(Class.CLASS_ID, field.datatype.properties.class_id)
-    .done(function (data) { class_ = data; })
+    .done(function (data) { self._class = data; })
     .always(function() {
       root.append(TEMPLATE);
+
+      var this_selector = selector + " > div.complex";
       
-      // Create controls
-      if (field.datatype.properties.collapsable == true) {
-        create_collapse(self, selector + " > div.complex > i");
+      // Collapse
+      if (field.datatype.properties.collapse == true) {
+        console.log("Collapse");
+        create_collapse(self, this_selector);
+        dfd.resolve();
+        return;
       }
       
+      // Popup
+      if (field.datatype.properties.mode == true) {
+        console.log("Popup");
+        create_popup(self, this_selector);
+        dfd.resolve();
+        return;
+      }
+
+      // Embedded
+      console.log("Embedded");
+      var complex = $(this_selector);
+      complex.append(DETAIL_TEMPLATE);
       self._detail = new Detail();
-      self._detail.init(selector + " > div.complex > div.detail", class_.object_fields)
+      self._detail.init(this_selector + " > div.detail", self._class.object_fields)
       .always(function() {
-        self._detail.visible(field.datatype.properties.collapsable == true ? false : true);
+        self._detail.visible(true);
         self._detail.edit(false);
         dfd.resolve();
       });
@@ -72,28 +152,34 @@ define(function (require) {
 
   Complex.prototype.commit = function() {
     //this._backup = this._detail.data();
-    this._detail.commit();
+    //this._detail.commit();
+    this._backup = JSON.parse(JSON.stringify(this._data));
   };
 
   Complex.prototype.restore = function() {
     //this._detail.data(this._backup);
-    this._detail.restore();
+    //this._detail.restore();
+    this._data = JSON.parse(JSON.stringify(this._backup));
   };
 
   Complex.prototype.edit = function(on) {
-    this._detail.edit(on);
+    //this._detail.edit(on);
+    this._edit = on;
   };
 
   Complex.prototype.data = function(values) {
     if (arguments.length == 0) {
-      return this._detail.data();
+      //return this._detail.data();
+      return this._data;
     } else {
-      this._detail.data(values);
+      //this._detail.data(values);
+      this._backup = values;
+      this._data = values;
     }
   };
 
   Complex.prototype.refresh = function() {
-    this._detail.refresh();
+    //this._detail.refresh();
   };
   
   Complex.renderer = function(field) {
